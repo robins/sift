81eaf81c23c89a5721f83563a2a3a4dfc39eb552
Robins Tharakan <tharakan@gmail.com>
2017-05-20 00:04:21 +1000

    change all.cmd to linux

--- all.cmd
+++ all.cmd
@@ -1,2 +1,6 @@
 
-psql.exe -1 -v ON_ERROR_STOP=1 -U postgres -h localhost -c "DROP SCHEMA IF EXISTS sift CASCADE" sift && psql.exe -1 -v ON_ERROR_STOP=1  -U postgres -h localhost -f sift_schema.sql sift && psql.exe -1 -v ON_ERROR_STOP=1  -U postgres -h localhost -f sift_func.sql sift && psql.exe -1 -v ON_ERROR_STOP=1  -U postgres -h localhost -f sift_usage.sql sift && psql.exe -1 -v ON_ERROR_STOP=1  -U postgres -h localhost -f sift_test.sql sift
+psql -1 -v ON_ERROR_STOP=1 -U postgres -c "DROP SCHEMA IF EXISTS sift CASCADE" sift \
+	&& psql -1 -v ON_ERROR_STOP=1 -U postgres -f sift_schema.sql sift \
+	&& psql -1 -v ON_ERROR_STOP=1 -U postgres -f sift_func.sql sift \
+	&& psql -1 -v ON_ERROR_STOP=1 -U postgres -f sift_usage.sql sift \
+	&& psql -1 -v ON_ERROR_STOP=1 -U postgres -f sift_test.sql sift
