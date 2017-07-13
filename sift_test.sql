6912b3097546698c3c1a12a21ea629a5bf902e44
Robins <tharakan@gmail.com>
2017-07-14 03:05:15 +1000

    Add some more tests

--- sift_test.sql
+++ sift_test.sql
@@ -19,3 +19,20 @@ SELECT * FROM GetPrioritizedTaskList(GetPlaceID('home'));
 SELECT * FROM GetPrioritizedTaskList(GetPlaceID('office'));
 
 COMMIT;
+
+
+WITH haves AS (
+  SELECT phraseto_tsquery('english', 'postgres split_words_into_rows') AS cond
+), havenots AS (
+  SELECT phraseto_tsquery('english', 'aws') AS cond
+), x AS (
+  SELECT unnest(string_to_array(b, ' ')) token 
+  FROM a, haves h, havenots hn
+  WHERE phraseto_tsquery('english', b) @> h.cond
+    AND NOT phraseto_tsquery('english', b) @> hn.cond
+)
+SELECT token, COUNT(*) c 
+FROM x, haves h
+WHERE NOT cond @> phraseto_tsquery('english', token)
+GROUP BY token 
+ORDER BY c DESC, token ASC ;
